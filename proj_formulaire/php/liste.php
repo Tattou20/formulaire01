5fc7784a3e74d566f29cd034f80d5dcd28c8c942
Arivola Ursulla <95035983+Tattou20@users.noreply.github.com>
2022-06-09 16:39:39 +0300

    Add files via upload

--- proj_formulaire/php/liste.php
+++ proj_formulaire/php/liste.php
@@ -5,62 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Utilisateurs</title>
-    <style>
-        body{
-            width: 50%;
-            font-family: arial;
-            margin: 5px auto;
-            padding: 20px;
-            color: black ; 
-            background: rgb(201, 219, 228);
-         }
-        div{
-            background: url(../img/bg1.jpg);
-            width: 800px;
-    
-        }
-        table{
-            border-collapse : collapse;
-            margin : 50px;
-            padding : 5px;
-            font-size : 0.9em;
-            font-family : sans-serif;
-            box-shadow : 0 0 20px rgba(0,0,0,0.15);
-            background : #ffffff;
-        }
-        td,th{
-            padding : 10px;
-            text-align : center;
-        }
-        thead{
-            padding : 30px;
-        }
-        tbody tr{
-            border-bottom : 1px solid #dddddd;
-        }
-        thead tr, tfoot tr{
-            background-color : black;
-            color : #ffffff;
-            text-align : center;
-        }
-        tbody tr.active-row{
-            font-weight : bold;
-            color : #009879;
-        }
-        tbody tr:nth-of-type(even) {
-            background-color : #f3f3f3;
-        }
-        input[type="submit"]{
-            background-color : aliceblue;
-            border : none;
-            border-radius : 3px;
-            color : black;
-            padding : 6px;
-            text-align : center;
-            font-weight : bold;
-            margin : 5px;
-        }
-    </style>
+    <link rel="stylesheet" type="text/css" href="../css/stylelist.css">
 </head>
 <body>
     <div>
@@ -94,7 +39,7 @@
 
             <td>
                 <form action="detail.php" method="POST">
-                    <input type="hidden" value="<?php echo $res["id"] ; ?>" />
+                    <input type="hidden" name="id" value="<?php echo $res["id"] ; ?>" />
                     <input type="submit" value="Détail" />
                 </form> 
                 <form action="delete.php" method="POST">
